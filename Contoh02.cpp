53c8f19d9ad530beda5bcbfae04795967296cdfc
Fabiannns <fabian4525024@univpancasila.ac.id>
2026-04-08 15:23:56 +0700

    Praktikum ke 2

---
+++ Contoh02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef int angka;
+typedef float pecahan;
+typedef char huruf[30];
+int main(){
+	angka umur;
+	pecahan pecah;
+	huruf nama;
+	huruf karakter;
+	cout<< "Masukan umur anda			:";cin>>umur;
+	cout<< "Umur anda adalah "<<umur<<endl;
+	cout<<endl;
+	cout<< "masukan bilangan pecahan	:";cin>>pecah;
+	cout<< "Bilangan pecahan "<<pecah<<endl;
+	cout<<endl;
+	cout<< "masukan satu huruf			:";cin>>karakter;
+	cout<< "Huruf Anda "<<karakter<<endl;
+cin.get();		}
